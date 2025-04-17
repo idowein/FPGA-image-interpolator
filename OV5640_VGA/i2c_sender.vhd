ffa8c2bf0c919ee463d3c2789812e6821152b866
idowein <ido.weinstock@gmail.com>
2025-04-17 17:20:58 +0300

    suiting i2c sender module to sda and scl

--- OV5640_VGA/i2c_sender.vhd
+++ OV5640_VGA/i2c_sender.vhd
@@ -10,10 +10,10 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity i2c_sender is
     Port ( clk   : in  STD_LOGIC;	 
-           siod  : inout  STD_LOGIC;
-           sioc  : out  STD_LOGIC;
-			  taken : out  STD_LOGIC;
-			  send  : in  STD_LOGIC;
+           sda  : inout  STD_LOGIC;
+           scl  : out  STD_LOGIC;
+		   taken : out  STD_LOGIC;
+		   send  : in  STD_LOGIC;
            id    : in  STD_LOGIC_VECTOR (7 downto 0);
            reg   : in  STD_LOGIC_VECTOR (7 downto 0);
            value : in  STD_LOGIC_VECTOR (7 downto 0));
@@ -29,9 +29,9 @@ begin
 		if busy_sr(11 downto 10) = "10" or 
 		   busy_sr(20 downto 19) = "10" or 
 		   busy_sr(29 downto 28) = "10"  then
-			siod <= 'Z';
+			sda <= 'Z';
 		else
-			siod <= data_sr(31);
+			sda <= data_sr(31);
 		end if;
 	end process;
 	
@@ -40,7 +40,7 @@ begin
 		if rising_edge(clk) then
 			taken <= '0';
 			if busy_sr(31) = '0' then
-				SIOC <= '1';
+				scl <= '1';
 				if send = '1' then
 					if divider = "00000000" then
 						data_sr <= "100" &   id & '0'  &   reg & '0' & value & '0' & "01";
@@ -55,52 +55,52 @@ begin
 				case busy_sr(32-1 downto 32-3) & busy_sr(2 downto 0) is
 					when "111"&"111" => -- start seq #1
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '1';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '1';
+							when "00"   => scl <= '1';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '1';
 						end case;
 					when "111"&"110" => -- start seq #2
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '1';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '1';
+							when "00"   => scl <= '1';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '1';
 						end case;
 					when "111"&"100" => -- start seq #3
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '0';
-							when "01"   => SIOC <= '0';
-							when "10"   => SIOC <= '0';
-							when others => SIOC <= '0';
+							when "00"   => scl <= '0';
+							when "01"   => scl <= '0';
+							when "10"   => scl <= '0';
+							when others => scl <= '0';
 						end case;
 					when "110"&"000" => -- end seq #1
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '0';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '1';
+							when "00"   => scl <= '0';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '1';
 						end case;
 					when "100"&"000" => -- end seq #2
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '1';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '1';
+							when "00"   => scl <= '1';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '1';
 						end case;
 					when "000"&"000" => -- Idle
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '1';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '1';
+							when "00"   => scl <= '1';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '1';
 						end case;
 					when others      => 
 						case divider(7 downto 6) is
-							when "00"   => SIOC <= '0';
-							when "01"   => SIOC <= '1';
-							when "10"   => SIOC <= '1';
-							when others => SIOC <= '0';
+							when "00"   => scl <= '0';
+							when "01"   => scl <= '1';
+							when "10"   => scl <= '1';
+							when others => scl <= '0';
 						end case;
 				end case;   
 
