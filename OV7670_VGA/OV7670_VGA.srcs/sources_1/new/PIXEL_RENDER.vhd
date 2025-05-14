5d29ac0a95976c142ff2d1aa1bd75b3edf3a79d0
Dvir Hershkovits <dvirhersh@gmail.com>
2025-05-14 16:05:50 +0300

    presentation v6.0
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- OV7670_VGA/OV7670_VGA.srcs/sources_1/new/PIXEL_RENDER.vhd
+++ OV7670_VGA/OV7670_VGA.srcs/sources_1/new/PIXEL_RENDER.vhd
@@ -5,34 +5,34 @@ use IEEE.std_logic_unsigned.all;
 
 entity pixel_render is
     Port (
-        pix_clk      : in  STD_LOGIC;
-        h_cnt        : in  std_logic_vector(11 downto 0);
-        v_cnt        : in  std_logic_vector(11 downto 0);
-        blank        : in  std_logic;
-        zoom_x2      : in  STD_LOGIC;
-        frame_fix    : in  STD_LOGIC_VECTOR (11 downto 0);
-        fr_address   : out std_logic_vector(18 downto 0);
-        vga_red      : out std_logic_vector(3 downto 0);
-        vga_blue     : out std_logic_vector(3 downto 0);
-        vga_green    : out std_logic_vector(3 downto 0);
-        cntl         : in std_logic
-    );
+        pix_clk    : in  STD_LOGIC;
+        h_cnt      : in  STD_LOGIC_VECTOR(11 downto 0);
+        v_cnt      : in  STD_LOGIC_VECTOR(11 downto 0);
+        blank      : in  STD_LOGIC;
+        zoom_x2    : in  STD_LOGIC;
+        frame_fix  : in  STD_LOGIC_VECTOR(11 downto 0);
+        fr_address : out STD_LOGIC_VECTOR(18 downto 0);
+        vga_red    : out STD_LOGIC_VECTOR(3  downto 0);
+        vga_blue   : out STD_LOGIC_VECTOR(3  downto 0);
+        vga_green  : out STD_LOGIC_VECTOR(3  downto 0);
+        cntl       : in  STD_LOGIC);
 end pixel_render;
 
 architecture Behavioral of pixel_render is
     signal val_tmp  : std_logic_vector(18 downto 0) := (others => '0'); -- Temporary for zoom calculation
     signal val_zoom : std_logic_vector(18 downto 0) := (others => '0'); -- Zoomed address calculation
     signal fr_addr  : std_logic_vector(18 downto 0) := (others => '0'); -- Frame address
-    signal bg_red   : std_logic_vector(3 downto 0);
-    signal bg_blue  : std_logic_vector(3 downto 0);
-    signal bg_green : std_logic_vector(3 downto 0);
+    signal bg_red   : std_logic_vector(3  downto 0);
+    signal bg_blue  : std_logic_vector(3  downto 0);
+    signal bg_green : std_logic_vector(3  downto 0);
     
-    constant CAMERA_WIDTH : integer := 640;  -- Camera width
+    constant CAMERA_WIDTH  : integer := 640; -- Camera width
     constant CAMERA_HEIGHT : integer := 480; -- Camera height
-    constant FRAME_WIDTH : integer := 640;   -- Width of the frame
-    constant FRAME_HEIGHT : integer := 480;  -- Height of the frame
+    constant FRAME_WIDTH   : integer := 640; -- Width of the frame
+    constant FRAME_HEIGHT  : integer := 480; -- Height of the frame
     
 begin
+
     fr_address <= fr_addr;
 
     -- Pixel Address and Zoom Calculation
@@ -40,7 +40,7 @@ begin
     begin
         if rising_edge(pix_clk) then
             if v_cnt >= CAMERA_HEIGHT then
-                val_tmp <= (others => '0');
+                val_tmp  <= (others => '0');
                 val_zoom <= (others => '0');
             else
                 if h_cnt < CAMERA_WIDTH then
@@ -51,7 +51,6 @@ begin
                     elsif h_cnt(0) = '0' then
                         val_tmp <= val_tmp + 1;
                     end if;
-
                     val_zoom <= val_zoom + 1;
                 end if;
             end if;
@@ -76,66 +75,67 @@ begin
         end if;
     end process;
     
-        process(pix_clk) begin
-            if rising_edge(pix_clk) then
-                if cntl = '1' then
-                    if blank = '1' then
-                        bg_red   <= "0000";
-                        bg_green <= "0000";
-                        bg_blue  <= "0000";
-                    else 
-                        bg_red   <= frame_fix(11 downto 8);
-                        bg_green <= frame_fix(7 downto 4);
-                        bg_blue  <= frame_fix(3 downto 0);
-                    end if;
-                else          
-                    if h_cnt < FRAME_WIDTH/8 then
-                        -- black
-                        bg_red <= "0000";
-                        bg_blue <= "0000";
-                        bg_green <= "0000";
-                    elsif(h_cnt >= FRAME_WIDTH/8 and h_cnt < FRAME_WIDTH/4) then
-                        -- blue
-                        bg_red <= "0000";
-                        bg_blue <= "1111";
-                        bg_green <= "0000";
-                    elsif(h_cnt >= FRAME_WIDTH/4 and h_cnt < FRAME_WIDTH/8 * 3) then
-                        -- green
-                        bg_red <= "0000";
-                        bg_blue <= "0000";
-                        bg_green <= "1111";
-                    elsif(h_cnt >= FRAME_WIDTH/8 * 3 and h_cnt < FRAME_WIDTH/2) then
-                        -- cyan
-                        bg_red <= "0000";
-                        bg_blue <= "1111";
-                        bg_green <= "1111";
-                    elsif(h_cnt >= FRAME_WIDTH/2 and h_cnt < FRAME_WIDTH/8 * 5) then
-                        -- red 
-                        bg_red <= "1111";
-                        bg_blue <= "0000";
-                        bg_green <= "0000";
-                    elsif(h_cnt >= FRAME_WIDTH/8 * 5 and h_cnt < FRAME_WIDTH/4 * 3) then
-                        -- magenta
-                        bg_red <= "1111";
-                        bg_blue <= "1111";
-                        bg_green <="0000";
-                    elsif(h_cnt >= FRAME_WIDTH/4 * 3 and h_cnt < FRAME_WIDTH/8 * 7) then
-                        -- yellow
-                        bg_red <= "1111";
-                        bg_blue <= "0000";
-                        bg_green <= "1111";
-                    elsif(h_cnt >= FRAME_WIDTH/8 * 7 and h_cnt < FRAME_WIDTH) then
-                        -- white 
-                        bg_red <= "1111";
-                        bg_blue <= "1111";
-                        bg_green <= "1111";
-                     end if;
-                 end if;
-            end if; 
-     end process;
+    process(pix_clk) begin
+        if rising_edge(pix_clk) then
+            if cntl = '1' then
+                if blank = '1' then
+                    bg_red   <= "0000";
+                    bg_green <= "0000";
+                    bg_blue  <= "0000";
+                else 
+                    bg_red   <= frame_fix(11 downto 8);
+                    bg_green <= frame_fix(7 downto 4);
+                    bg_blue  <= frame_fix(3 downto 0);
+                end if;
+            else          
+                if h_cnt < FRAME_WIDTH/8 then
+                    -- black
+                    bg_red   <= "0000";
+                    bg_blue  <= "0000";
+                    bg_green <= "0000";
+                elsif(h_cnt >= FRAME_WIDTH/8 and h_cnt < FRAME_WIDTH/4) then
+                    -- blue
+                    bg_red   <= "0000";
+                    bg_blue  <= "1111";
+                    bg_green <= "0000";
+                elsif(h_cnt >= FRAME_WIDTH/4 and h_cnt < FRAME_WIDTH/8 * 3) then
+                    -- green
+                    bg_red   <= "0000";
+                    bg_blue  <= "0000";
+                    bg_green <= "1111";
+                elsif(h_cnt >= FRAME_WIDTH/8 * 3 and h_cnt < FRAME_WIDTH/2) then
+                    -- cyan
+                    bg_red   <= "0000";
+                    bg_blue  <= "1111";
+                    bg_green <= "1111";
+                elsif(h_cnt >= FRAME_WIDTH/2 and h_cnt < FRAME_WIDTH/8 * 5) then
+                    -- red 
+                    bg_red   <= "1111";
+                    bg_blue  <= "0000";
+                    bg_green <= "0000";
+                elsif(h_cnt >= FRAME_WIDTH/8 * 5 and h_cnt < FRAME_WIDTH/4 * 3) then
+                    -- magenta
+                    bg_red   <= "1111";
+                    bg_blue  <= "1111";
+                    bg_green <="0000";
+                elsif(h_cnt >= FRAME_WIDTH/4 * 3 and h_cnt < FRAME_WIDTH/8 * 7) then
+                    -- yellow
+                    bg_red   <= "1111";
+                    bg_blue  <= "0000";
+                    bg_green <= "1111";
+                elsif(h_cnt >= FRAME_WIDTH/8 * 7 and h_cnt < FRAME_WIDTH) then
+                    -- white 
+                    bg_red   <= "1111";
+                    bg_blue  <= "1111";
+                    bg_green <= "1111";
+                end if;
+            end if;
+        end if; 
+    end process;
 
     -- Output Assignment
-    vga_red <= bg_red;
-    vga_blue <= bg_blue;
+    vga_red   <= bg_red;
+    vga_blue  <= bg_blue;
     vga_green <= bg_green;
+
 end Behavioral;
